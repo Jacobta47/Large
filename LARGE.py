5cd2a32bca2355eef0f6178887ee292ef6861801
Jacob T. Armentrout <jacoba973@gmail.com>
2021-03-27 19:05:40 -0400

    Updated Work

--- LARGE.py
+++ LARGE.py
@@ -4,27 +4,28 @@
 
 import os
 
-open('Large.txt', 'w').close()
+filename = 'large.txt'
 
-File1 = open(r"C:\Users\jacob\Large.txt","w+")
+open(filename, 'x').close()
+
+f = open(filename, 'w+')
 
 str1 = "1.0."
 
 i = 0
 
-u = "/9999999999"
+u = 1000000000
 
 def size():
-    return os.path.getsize("Large.txt")
+    return os.path.getsize("large.txt")
 
-end = int(size())
+while i < u:
+    f.write(str1)
+    i += 1
+    end = "Size: " +  '{:,.0f}'.format(size()) + " bytes"
+    out = "Progress: " + '{:,.0f}'.format(i) + " / " + '{:,.0f}'.format(u)
+    print(out + " | " + end)
 
-print(end)
+f.close()
+print(end + " finished.")
 
-while i < 9999999999:
-    File1.write(str1)
-    i += 9
-    print(str(i) + u)
-
-size()
-print(end)
